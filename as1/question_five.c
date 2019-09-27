f763598bddb8c24b124184fdb223f4827aaf461e
Logan Saso <sasol@oregonstate.edu>
2019-09-27 12:20:21 -0700

    Assignment done?

--- as1/question_five.c
+++ as1/question_five.c
@@ -1,4 +1,41 @@
-int main() {
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+
+    if (fork() == 0)
+    {
+        // child process
+        char* args[3];
+        args[0] = (char*) "-l";
+        args[1] = (char*) "-F";
+        args[2] = (char*) "-h";
+        // Passes params to the func from the arbitrarily-long list of arguments
+        if(execl("ls", "-l", "-F", "-h") == -1) {
+            printf("Execl had an error.\n");
+        }
+        // Passes params to the func (including funcs from path) from the arbitrarily-long list of arguments
+        if(execlp("ls", "-l", "-F", "-h") == -1) {
+            printf("Execlp had an error.\n");
+        }
+        // Passes params to the func from an array of null-terminated strings
+        if (execv("ls", args) == -1) {
+            printf("Execv had an error.\n");
+        }
+        // Passes params to the func (including funcs from path) from an array of null-terminated strings
+        if (execvp("ls", args) == -1) {
+            printf("Execvp had an error.\n");
+        }
+    }
+    else
+    {
+        //parent process
+        // Wait for the child process to exit
+        wait(NULL);
+    }
 
     return 0;
 }
--- as1/question_four.c
+++ as1/question_four.c
@@ -1,4 +1,22 @@
-int main() {
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+
+    if (fork() == 0)
+    {
+        // child process
+        printf("hello\n");
+    }
+    else
+    {
+        //parent process
+        wait(NULL);
+        printf("goodybye\n");
+    }
 
     return 0;
 }
