e2ca94a84667020134f03854e393f8b4ff562efa
Logan Saso <sasol@oregonstate.edu>
2019-09-30 00:35:40 -0700

    Added xv6 source code

--- as1/question_five.c
+++ as1/question_five.c
@@ -8,25 +8,35 @@ int main()
     if (fork() == 0)
     {
         // child process
-        char *args[3];
-        args[0] = (char *)"-l";
-        args[1] = (char *)"-F";
-        args[2] = (char *)"-h";
+
         // Passes params to the func from the arbitrarily-long list of arguments
+        // Replaces current process context with `ls`, and returns with a failure since ls isn't in the local dir (path not considered)
         if (execl("ls", "-l", "-F", "-h", NULL) == -1)
         {
             printf("Execl had an error.\n");
         }
+
         // Passes params to the func (including funcs from path) from the arbitrarily-long list of arguments
+        // Replaces current process context with `ls`, never returns, since it doesn't fail
         if (execlp("ls", "-l", "-F", "-h", NULL) == -1)
         {
             printf("Execlp had an error.\n");
         }
+
+        char *args[4];
+        args[0] = (char *)"-l";
+        args[1] = (char *)"-F";
+        args[2] = (char *)"-h";
+        args[3] = NULL;
+
         // Passes params to the func from an array of null-terminated strings
+        // Replaces current process context with `ls`, and returns with a failure since ls isn't in the local dir (path not considered)
         if (execv("ls", args) == -1)
         {
             printf("Execv had an error.\n");
         }
+
+        // Replaces current process context with `ls`, never returns, since it doesn't fail
         // Passes params to the func (including funcs from path) from an array of null-terminated strings
         if (execvp("ls", args) == -1)
         {
