bb2f04eb1b63ffb4d7d034f7e6f1496502bace40
Logan Saso <sasol@oregonstate.edu>
2019-10-06 15:40:26 -0700

    Not joining blocks for some reason

--- as2/minimal_test.c
+++ as2/minimal_test.c
@@ -4,8 +4,15 @@
 int main()
 {
     void *otherptr = beavalloc(1024);
+    beavalloc_dump(FALSE);
     beavfree(otherptr);
     void *smallptr = beavalloc(256);
+    beavalloc_dump(FALSE);
     void *smallptr2 = beavalloc(512);
+    beavalloc_dump(FALSE);
+    beavfree(smallptr2);
+    beavalloc_dump(FALSE);
+    beavfree(smallptr);
+    beavalloc_dump(FALSE);
     printf("%p, %p, %p\n", otherptr, smallptr, smallptr2);
 }
