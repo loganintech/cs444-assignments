5eded1a004eb296659fdeda200a4ef5a98076894
Logan Saso <sasol@oregonstate.edu>
2019-11-20 16:38:23 -0800

    no pid

--- as5/proc.c
+++ as5/proc.c
@@ -299,10 +299,8 @@ int kthread_join(benny_thread_t tid)
       if (p->state == ZOMBIE)
       {
         // Found one.
-        pid = p->pid;
         kfree(p->kstack);
         p->kstack = 0;
-        p->pid = 0;
         p->parent = 0;
         p->name[0] = 0;
         p->killed = 0;
